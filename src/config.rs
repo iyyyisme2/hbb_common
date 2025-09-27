89be80f1c450819e13a5a30c951e38a7c2a1f338
iyyyisme2 <andlinuxx@gmail.com>
2025-09-28 00:33:34 +0800

    hide tray true

--- src/config.rs
+++ src/config.rs
@@ -72,7 +72,7 @@ lazy_static::lazy_static! {
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = { let mut map = HashMap::new();
                          map.insert("password".to_string(), "The_rustdesk_is_best666".to_string());RwLock::new(map)};
     //pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = { let mut map = HashMap::new();                                map.insert("OPTION_HIDE_TRAY".to_string(), "Y".to_string());RwLock::new(map)};
+pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = { let mut map = HashMap::new();                                map.insert("OPTION_HIDE_TRAY".to_string(), "Y".to_string());RwLock::new(map)};
 }
 
 lazy_static::lazy_static! {
